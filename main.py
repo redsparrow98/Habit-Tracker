9b0b1a8b2fe8b1f5306ec47df619a90740db1e2a
prosi <prosicnatalija98@gmail.com>
2024-06-23 20:13:17 +0100

    - Added step 4 & 5 to the program.
    - 4) PUT request - update an existing pixel with new data for tracking.
    
    - 5) DELETE request - Delete a pixel from the DataBase if one exists with the unique date key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import find_dotenv, load_dotenv
 import os
+from datetime import datetime
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ================= #
@@ -128,25 +129,97 @@ graph_config = {
 * First the endpoint is slightly different to the one used to create the graph at the end of the endpoint the final 
 path is the GraphID that was provided in the graph_config dictionary under the 'id' key.
 
-* Documentation (https://docs.pixe.la/entry/post-pixel) specifys how to configurate the data for the pixel.
+* Documentation (https://docs.pixe.la/entry/post-pixel) specify how to configurate the data for the pixel.
 Two requiered keys are:
 - date (representing the date on which you with to enter the data in the format of: YYYYMMDD)
-- quantity (Specify the quantity to be registered on the specified date )
+- quantity (Specify the quantity to be registered on the specified date)
+(0.1 of an hour is 6min so 2h30min = 2.5 / 2h40min = 2.66 etc)
+
+* In order for the date to automatically be detected for the day of use the program uses datetime module and formats the
+now() result by using the strftime() Method (https://www.w3schools.com/python/python_datetime.asp)
+creating the requiered inout of the date yyyymmdd to be passed in to the POST request.
+
+- Or if we want to backdate and input a different date the we can create a specific date object like this:
+date = datetime(year=2024, month=6, day=22)
+then the date gets formated using the date.strftime("%Y%m%d") and passed in to the request
+
 
 * This post requires the Token to be passed in the header for authentication before the post is allowed, since this was
 used in step 2 as well this part will reuse the 'header' dictionary contain the authentication token for the PixelaAPI
 
+* For the purposes of step 4 and 5 updating and deleting the pixel the create pixel request will be commented out
 """
 
+# The graph ID that will be used to CREATE, UPDATE & DELETE pixels of the graph for this program
+graph_id = graph_config["id"]
+
 # This endpoint is to post a pixel to a specific graph - format (/v1/users/<username>/graphs/<graphID>)
-pixel_creation_endpoint = f"{pixela_endpoint}/{PIXELA_USERNAME}/graphs/graph1"
+pixel_creation_endpoint = f"{pixela_endpoint}/{PIXELA_USERNAME}/graphs/{graph_id}"
+
+# For the date to be filled out automatically for every day to save manual input use date time.
+# To format the date to fit the PixelaAPI 'date' key format requirements use the strftime() method
+# (https://www.w3schools.com/python/python_datetime.asp)
+date = datetime.now()
+formated_date = date.strftime("%Y%m%d")
 
 # Requiered parameters to create the pixel on the graph (date(yyyymmdd), quantity: Float [0-9].[0-9])
 pixel_conf = {
-    "date": "20240622",
-    "quantity": "2.0",
+    "date": formated_date,
+    "quantity": "1",
 }
 
-# Request for the pixel creation passing on the pixel_par to be saved on the graph
-pixel_creation = requests.post(url=pixel_creation_endpoint, json=pixel_conf, headers=headers)
-print(pixel_creation.text)
+# # Post request for the pixel creation passing on the pixel_par to be saved on the graph
+# pixel_creation = requests.post(url=pixel_creation_endpoint, json=pixel_conf, headers=headers)
+# print(pixel_creation.text)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 4th STEP UPDATING A PIXEL ON THE GRAPH
+
+"""
+* To update the data in a pixel the request uses the PUT method.
+
+* Each pixel has a unique key identifying it in the DB(database) for this API it is the date.
+- To update a pixel we must know what date we want to update in the graph, so the endpoint for updating is the
+Pixela endpoint/ username/ graphs/ graph_id / date of teh pixel we want to update.
+
+* The parameters that are being passed is the 'quantity' which will update the already existing quantity value with the
+new value being passed in the PUT request
+
+* The request is commented out once action is complete to avoid errors
+"""
+
+# Date if the pixel you wish to update
+update_date = "20240620"
+
+# Endpoint for the updating an existing pixel on a graph
+update_pixel_endpoint = f"{pixela_endpoint}/{PIXELA_USERNAME}/graphs/{graph_id}/{update_date}"
+
+# New quantity to replace/update the original data in the pixel DB(database)
+update_params = {
+    "quantity": "3.8"
+}
+
+# # Put request to update an existing pixel with different data. Each pixel has a special date that identifies them
+# update_response = requests.put(url=update_pixel_endpoint, json=update_params, headers=headers)
+# print(update_response.text)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# 5th STEP DELETING AN EXISTING PIXEL FROM THE GRAPH ENTIRELY
+
+"""
+* To delete a pixel from the DB(database) we use the DELETE request.
+
+* Same as in step 4 to access a specific pixel we need the unique key which is the date. This way the program and server 
+knows exactly which entry we want to delete. So the endpoint is the same as updating a pixel since we need access to a
+specific pixel in the DB.
+
+* To delete the pixel we simply use the DELETE request and pass in the endpoint with the graph id and the pixel key as 
+well as the header to authenticate the user. This will delete the pixel if one exists with such a date key.
+
+* The delete request is commented out after action is complete to avoid errors.
+"""
+
+# # Delete request to delete a pixel from the DB(database) fully
+# delete_pixel_response = requests.delete(url=update_pixel_endpoint, headers=headers)
+# print(delete_pixel_response.text)
